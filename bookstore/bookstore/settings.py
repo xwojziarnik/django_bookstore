597f56b69706fe2ecd7006acd657cf1fb2d6ddba
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-19 19:29:38 +0200

    Trying next page..

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9n8rd@nbj1tka(6xo*)&tc-&^ruc2rcril#12e8km1kgfti*@3'
 
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-9n8rd@nbj1tka(6xo*)&tc-&^ruc2rcril#12e8km1kgfti*@3')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['141.144.237.60', 'localhost']
+ALLOWED_HOSTS = ['141.144.237.60', '127.0.0.1']
 
 
 # Application definition
@@ -120,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
