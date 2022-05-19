0c9230f5887b41ca66dd927f210ccd7d6067e90e
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-19 18:11:05 +0200

    Trying on oracle

--- bookstore/Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn bookstore.wsgi
--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +126,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku:
-django_heroku.settings(locals())
