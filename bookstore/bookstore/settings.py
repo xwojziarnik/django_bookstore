14b2fa41c1a4e12e8648e849a0edec0664397c57
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-19 16:28:49 +0200

    Heroku initialising

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku:
+django_heroku.settings(locals())
